f4f39360e9880f4f2a327fc89b0503e7113e2bfe
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-03 11:26:39 -0300

    Implementado step 7 do Walkthrough SAPUI5

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function(Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
 	"use strict";
-
+	
 	return Controller.extend("ui5.walkthrough.controller.App", {
+		onInit() {
+			const oData = {
+				recipient: {
+					name: 'World'
+				}
+			};
+			
+			const oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+		
         onShowHello() {
-            MessageToast.show("Hello World");
+			MessageToast.show("Hello World");
         }
 	});
-});
+}); 
