6855c627817d7c1628587e253ae0037ecdb6c57f
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-09 11:05:53 -0300

    Implementado step 30 do Walkthrough SAPUI5

--- webapp/Component.js
+++ webapp/Component.js
@@ -27,6 +27,8 @@ sap.ui.define([
                 bundleName: "ui5.walkthrough.i18n.i18n"
             });
             this.setModel(i18nModel, "i18n");
+
+            this.getRouter().initialize();
         },
 
     })
